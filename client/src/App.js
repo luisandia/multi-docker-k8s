395e7b50fdb95c6ace47be771f384fc8ae89f1e8
luisandia <luisandiamamani@gmail.com>
2020-01-14 02:34:12 -0500

    updated header

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
           >
             Learn React
           </a> */}
-          <Link to="/" style={{marginRight: '10vw'}}>Home in Kubernetes!</Link>
+          <Link to="/" style={{marginRight: '10vw'}}>Fib Calculator kubernetes!</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
