1fc33c923603be3a594d93b342fe50ef2e7315aa
luisandia <luisandiamamani@gmail.com>
2020-01-18 01:02:38 -0500

    restore scripts to push containers and  change email

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
-# docker build -t luisandia/multi-client:latest -t luisandia/multi-client:$SHA -f ./client/Dockerfile ./client
-# docker build -t luisandia/multi-server:latest -t luisandia/multi-server:$SHA -f ./server/Dockerfile ./server
-# docker build -t luisandia/multi-worker:latest -t luisandia/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t luisandia/multi-client:latest -t luisandia/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t luisandia/multi-server:latest -t luisandia/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t luisandia/multi-worker:latest -t luisandia/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-# docker push luisandia/multi-client:latest
-# docker push luisandia/multi-client:$SHA
-# docker push luisandia/multi-server:latest
-# docker push luisandia/multi-server:$SHA
-# docker push luisandia/multi-worker:latest
-# docker push luisandia/multi-worker:$SHA
+docker push luisandia/multi-client:latest
+docker push luisandia/multi-client:$SHA
+docker push luisandia/multi-server:latest
+docker push luisandia/multi-server:$SHA
+docker push luisandia/multi-worker:latest
+docker push luisandia/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=luisandia/multi-server:$SHA
